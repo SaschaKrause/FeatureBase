11a7a87724530d3084d958a789d241381214b38f
Sascha Krause <mr.sascha.krause@gmail.com>
2014-07-27 14:51:19 +0200

    created page creater

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,11 +3,17 @@
 var _ = require('lodash')
   , gulp = require('gulp')
   , jade = require('gulp-jade')
-  , gutil = require('gulp-util')
+  , util = require('gulp-util')
   , ngmin = require('gulp-ngmin')
   , jshint = require('gulp-jshint')
   , stylish = require('jshint-stylish')
+  , notify = require("gulp-notify")
   , prompt = require('gulp-prompt')
+  , template = require('gulp-template')
+  , rename = require("gulp-rename")
+  , gulpMultinject = require('gulp-multinject')
+  , inject = require('gulp-inject')
+  , runSequence = require('run-sequence')
   , stylus = require('gulp-stylus');
 
 
@@ -71,25 +77,76 @@ gulpfile.globals._ = true;
 ------------------------------------------------------------------------------*/
 gulp.task('default', ['serve']);
 gulp.task('serve', ['css:stylus', 'jade', 'watch']);
-gulp.task('page', ['create:page']);
-// gulp.task('serve', ['css:stylus', 'js:serve', 'watch']);
 
 
-// Get and render all .styl files recursively
+// gulp do:page --name pageName (camelCase)
+gulp.task('do:page', function () {
+
+  console.log(util.colors.green('Using Page Name: ' + util.env.name)); 
+
+  if(util.env.name === undefined) {
+    console.log(util.colors.red('you need to specify a page name (e.g --name DetailListOverview)')); 
+    return null;
+  }
+
+  // TODO: stop if page already exist
+
+  var pageRoot = 'app-src/pages/';
+  var fileName = getDashedStringFromCamelCase(util.env.name);
+  var controllerName = util.env.name+'Page';
+  var notifiyRoute = "{state: 'LAYOUT.STATE', url: '/ROUTE', views: {'content': {templateUrl: 'app-build/pages/"+fileName+"/page_"+fileName+".html', controller: '"+controllerName+"'} } },";
+  var notifiyStylus = "@import './pages/"+fileName+"/page_"+fileName+".styl'";
+
+    // create js page
+    gulp.src('templates/pages/page.js')
+        .pipe(template({
+          name: controllerName, 
+          createdAt: new Date()
+        }))
+        .pipe(rename('page_'+fileName+".js"))
+        .pipe(gulp.dest('public/'+pageRoot+fileName))
+        
+        // after page is created, add it to the index jade file 
+        .on('end', function () {
+          // ref page in jade file
+          gulp.src('views/index_debug.jade')
+            .pipe(inject(gulp.src('public/'+pageRoot+"/**/*.js", {read: false}), {name: 'pageController', ignorePath: "public/"}))
+            .pipe(gulp.dest('views/'));
 
 
-gulp.task('create:page', function () {
+          console.log(util.colors.green('\n------------------------\nToDo:\n------------------------'));
+          console.log(util.colors.yellow('add to routes.cfg:'));
+          console.log(util.colors.green(notifiyRoute));
+        });
 
-  gulp.src('templates/pages/**/*.js')
-    .pipe(prompt.prompt({
-        type: 'input',
-        name: 'task',
-        message: 'Which task would you like to run?'
-    }, function(res){
-        //value is in res.task (the name option gives the key)
-        return res.task;
-    })).
-    pipe(gulp.dest('dest'));
+
+    // create jade page
+    gulp.src('templates/pages/page.jade')
+        .pipe(template({
+          pageContentClass: fileName, 
+          createdAt: new Date()
+        }))
+        .pipe(rename('page_'+fileName+".jade"))
+        .pipe(gulp.dest('public/'+pageRoot+fileName))
+        .on('end', function() {
+          runSequence('jade');
+        });
+        
+
+    // create stylus page
+    gulp.src('templates/pages/page.styl')
+        .pipe(template({
+          pageContentClass: fileName, 
+          createdAt: new Date()
+        }))
+        .pipe(rename('page_'+fileName+".styl"))
+        .pipe(gulp.dest('public/'+pageRoot+fileName))
+        .on('end', function() {
+          console.log(util.colors.yellow('add to app.styl:'));
+          console.log(util.colors.green(notifiyStylus));
+          runSequence('css:stylus');
+        })
+        
 
 });
 
@@ -127,24 +184,32 @@ gulp.task('watch', function () {
 
 
 
+/** helpers
+------------------------------------------------------------------------------*/
+
+function getDashedStringFromCamelCase(camelCased) {
+  var unCapitalized = camelCased.charAt(0).toLowerCase() + camelCased.slice(1);
+  return unCapitalized.replace(/([A-Z])/g, function($1){return "-"+$1.toLowerCase();});
+}
+
 /** Errors
 ------------------------------------------------------------------------------*/
 
 function err() {
   /*jshint validthis:true */
-  gutil.beep();
+  util.beep();
   this.emit('end');
 }
 
 function jadeErr(err) {
   /*jshint validthis:true */
   console.log('\n', err.message, '\n');
-  gutil.beep();
+  util.beep();
   this.emit('end');
 }
 
 function gulpChanged() {
-  gutil.beep();
-  console.log(gutil.colors.red('\n------------------------\nRestart the Gulp process\n------------------------'));
+  util.beep();
+  console.log(util.colors.red('\n------------------------\nRestart the Gulp process\n------------------------'));
   process.exit(0);
 }
--- public/app-src/js/configs/routes-cfg.js
+++ public/app-src/js/configs/routes-cfg.js
@@ -21,8 +21,9 @@ angular.module('config.routes', []).constant('routesCfg', [
 		// releases
 		{state: 'list.project-overview',										url: '/project/overview',																	views: {'content': {templateUrl: 'app-build/pages/project-overview/page_project-overview.html',													controller: 'ProjectOverviewPage' } } },
 
-		{state: 'detail.feature',														url: '/feature/{featureId}',															views: {'content': {templateUrl: 'partials/counter-detail_release',									controller: 'FeatureDetailCtrl' } } },
+		// {state: 'detail.feature',														url: '/feature/{featureId}',															views: {'content': {templateUrl: 'partials/counter-detail_release',									controller: 'FeatureDetailCtrl' } } },
 
 		{state: 'app.profile',															url: '/profile',																					views: {'content': {templateUrl: 'partials/profile',																controller: 'ProfileCtrl' } } },
-		{state: 'app.initialisation-main',									url: '/init',																							views: {'content': {templateUrl: 'partials/init',																		controller: 'ProfileCtrl'} } },
+		{state: 'list.initialisation-main',									url: '/init',																							views: {'content': {templateUrl: 'partials/init',																		controller: 'ProfileCtrl'} } },
+		{state: 'list.feature-detail', url: '/features', views: {'content': {templateUrl: 'app-build/pages/feature-detail/page_feature-detail.html', controller: 'FeatureDetailPage'} } },
 	]);
